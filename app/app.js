5fb08f46d0aae2740db5808545d63f5bcc48613f
Seiji Nakaya <snakaya@loosedays.jp>
2020-12-12 12:00:59 +0900

    initial import

---
+++ app/app.js
@@ -0,0 +1,24 @@
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;
+
+app.get('/',function(req,res){
+    res.json({
+        company_name:"LOOSEDAYS Co.,Ltd.",
+        email:"info@loosedays.jp"
+    });
+});
+
+app.use(function(err, req, res, next){
+    console.log(err.stack);
+    return res.status(500).send('Internal Server Error');
+});
+
+app.listen(port, 'localhost', function() {
+    console.log(`Server running at http://localhost:${port}/`)
+});
